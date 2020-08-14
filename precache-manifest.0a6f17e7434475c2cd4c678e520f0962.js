62e0fe3054476ad64159f444f988e3035c01c046
Rm Hridoy <55066846+rmhridoy@users.noreply.github.com>
2020-08-14 16:47:43 +0600

    Updates

---
+++ precache-manifest.0a6f17e7434475c2cd4c678e520f0962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ba7284a4d621e8abbca2e91249b8be",
+    "url": "/todo-apps-react-firebase/index.html"
+  },
+  {
+    "revision": "18d42d3f4a38f371d17b",
+    "url": "/todo-apps-react-firebase/static/css/main.ccb71ad7.chunk.css"
+  },
+  {
+    "revision": "aca330a916d2ac5f9792",
+    "url": "/todo-apps-react-firebase/static/js/2.de0b5ce2.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/todo-apps-react-firebase/static/js/2.de0b5ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d42d3f4a38f371d17b",
+    "url": "/todo-apps-react-firebase/static/js/main.c274cc6e.chunk.js"
+  },
+  {
+    "revision": "3597a063e706ba5e82a8",
+    "url": "/todo-apps-react-firebase/static/js/runtime-main.d6236735.js"
+  }
+]);
